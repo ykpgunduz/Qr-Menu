b979adf40e28ab60e130754ff9546c0f080af73c
yakupgit <137324492+ykpgunduz@users.noreply.github.com>
2024-12-19 14:17:26 +0300

    19.12.2024.7

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,16 +2,19 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        // if($this->app->environment('production')) {
-        //     \URL::forceScheme('https');
-        // } else {
-        //     dd("Geliştirici ile irtibata geçiniz..");
-        // }
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        } elseif ($this->app->environment('local')) {
+            logger('Yerel geliştirme ortamı algılandı.');
+        } else {
+            abort(500, 'Bilinmeyen ortam algılandı. Lütfen geliştirici ile iletişime geçin.');
+        }
     }
 }
