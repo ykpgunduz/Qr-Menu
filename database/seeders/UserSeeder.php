034fcedd7b8b7f510cab1463e5b25578bc2943da
yakupgit <137324492+ykpgunduz@users.noreply.github.com>
2025-01-09 21:04:27 +0300

    Notifiable.1

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,24 +12,17 @@ class UserSeeder extends Seeder
     {
         $users = [
             ['name' => 'Developer', 'email' => 'developer@harpysocial.com', 'password' => 'asd'],
-            ['name' => 'Kasa', 'email' => 'kasa@harpysocial.com', 'password' => 'Kasa123',  ],
+            ['name' => 'Kasa', 'email' => 'kasa@harpysocial.com', 'password' => 'Kasa123'],
             ['name' => 'Patron', 'email' => 'patron@harpysocial.com', 'password' => 'Patron123'],
-            ['name' => 'Garson', 'email' => 'garson@harpysocial.com', 'password' => 'Garson123'],
+            ['name' => 'Garson', 'email' => 'garson@harpysocial.com', 'password' => 'Garson123']
         ];
 
         foreach ($users as $user) {
             $createdUser = User::create([
                 'name' => $user['name'],
                 'email' => $user['email'],
-                'password' => Hash::make($user['password']),
+                'password' => Hash::make($user['password'])
             ]);
-
-            // Bildirim ilişkisinin oluşturulması
-            // notifiable_type'ı 'App\Models\User' olarak ayarlıyoruz ve notifiable_id olarak kullanıcının id'sini veriyoruz
-            // $createdUser->notifiable()->create([
-            //     'notifiable_type' => 'App\Models\User',
-            //     'notifiable_id' => $createdUser->id,
-            // ]);
         }
     }
 }
