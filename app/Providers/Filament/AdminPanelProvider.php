e55637c4f7871c00830e9e9189f0c1e702488d9f
yakupgit <137324492+ykpgunduz@users.noreply.github.com>
2025-01-14 22:12:09 +0300

    14.01.2025v5

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors(['primary' => Color::Sky])
-            ->favicon(asset('images/favicon.png'))
+            ->favicon(app()->environment('production') ? secure_asset('images/favicon.png') : asset('images/favicon.png'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([Pages\Dashboard::class])
