8b2ff3f7e1b07c00da5d54841cfe3da3780ac09a
yakupgit <137324492+ykpgunduz@users.noreply.github.com>
2025-01-21 19:31:30 +0300

    env

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Underground'),
+    'name' => env('APP_NAME', 'Harpy QR Otomasyon'),
 
     /*
     |--------------------------------------------------------------------------
