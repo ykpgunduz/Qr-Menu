460a980e9f7ee36c7e2c68b6d260f3082c6a0237
yakupgit <137324492+ykpgunduz@users.noreply.github.com>
2025-01-28 12:38:46 +0300

    edited

--- resources/views/partials/product-details.blade.php
+++ resources/views/partials/product-details.blade.php
@@ -1,5 +1,5 @@
 <div class="product-detail">
-    <img src="{{ $product->thumbnail ? asset(path: 'storage/' . $product->thumbnail) : asset('img/kafe-logo.png') }}" alt="{{ $product->title }}" class="img-fluid mb-3 rounded">
+    <img src="{{ $product->thumbnail ? asset(path: 'storage/img/' . $product->thumbnail) : asset('img/kafe-logo.png') }}" alt="{{ $product->title }}" class="img-fluid mb-3 rounded">
     <h3 class="text-center">{{ $product->title }}</h3>
     <p class="text-muted">{{ $product->body }}</p>
     <h4 class="text-primary text-center">{{ $product->price }}₺</h4>
